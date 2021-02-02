86447b7a7fa61b63dbd04f85cd8b30e3e0d12dfb
MaryWenzel <wenzel.mary1993@gmail.com>
2021-02-02 17:20:21 -0600

    first commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Container,
+} from "@material-ui/core";
 
 const Import = (props) => {
-    // fill out this component
+  // fill out this component
 
-    return (
-        <p>Import Component</p>
-    )
-}
+  return (
+    <Container>
+      <p>Import Component</p>
+      <Button variant="contained" color="primary">
+        Import
+      </Button>
+      <Table aria-label="makes-table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((makes) => (
+            <TableRow key={makes.id}>
+              <TableCell>{makes.MakeId}</TableCell>
+              <TableCell>{makes.VehicleTypeId}</TableCell>
+              <TableCell>{makes.VehicleTypeName}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
 
-export default Import
+export default Import;
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,11 @@
 // fill out this container
+import { connect } from "react-redux";
+import Import from "../components/Import";
+
+const mapStateToProps = (state) => {
+  return {
+    makes: state.makes,
+  };
+};
+
+export default connect(mapStateToProps)(Import);
